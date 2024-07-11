e5fd74ac4fcf960d1162f08cacd56b8cba95b21e
devdongwoo <gimdongwoo64@gmail.com>
2024-07-11 12:28:13 +0900

    프로젝트 초기 설정

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default function App() {
+   const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+}
+
---
+++ src/main/java/com/dorandoran/backend/Controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.dorandoran.backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
