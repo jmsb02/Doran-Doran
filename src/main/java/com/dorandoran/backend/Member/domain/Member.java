78c2aea8d2b76d0996d1e785e0a048625fe27338
jaexxin <jmsb02@naver.com>
2024-07-14 18:19:15 +0900

    7/14 backand entity upload

--- src/main/java/com/dorandoran/backend/Member/domain/Member.java
+++ src/main/java/com/dorandoran/backend/Member/domain/Member.java
@@ -1,18 +1,23 @@
 package com.dorandoran.backend.Member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Member {
 
     @Id @GeneratedValue
-    @JoinColumn(name = "member_id")
+    @Column(name = "member_id")
     private Long member_id;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 }
