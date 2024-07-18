7f4930b3c0f502d86063e0bfa384950937634a8a
jaexxin <jmsb02@naver.com>
2024-07-18 10:11:57 +0900

    718 Member and Post Entity

--- src/main/java/com/dorandoran/backend/Member/Model/Member.java
+++ src/main/java/com/dorandoran/backend/Member/Model/Member.java
@@ -1,4 +1,36 @@
 package com.dorandoran.backend.Member.Model;
 
+import com.dorandoran.backend.Post.Model.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
 public class Member {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany
+    @JoinColumn(name = "member_id")
+    private List<Post> posts = new ArrayList<>();
 }
--- src/main/java/com/dorandoran/backend/Post/Model/Post.java
+++ src/main/java/com/dorandoran/backend/Post/Model/Post.java
@@ -1,4 +1,30 @@
 package com.dorandoran.backend.Post.Model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
 public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at;
 }
