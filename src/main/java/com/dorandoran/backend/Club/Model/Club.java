285e59f58176b883e120a26bda92415f073771e3
CHAEYEON KIM <gimchaeyeon57@gmail.com>
2024-07-20 15:17:58 +0900

    Update Entity

--- src/main/java/com/dorandoran/backend/Club/Model/Club.java
+++ src/main/java/com/dorandoran/backend/Club/Model/Club.java
@@ -1,18 +1,16 @@
 package com.dorandoran.backend.Club.Model;
 
 import com.dorandoran.backend.Marker.Model.Marker;
+import com.dorandoran.backend.common.JpaBaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Club {
+public class Club extends JpaBaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,10 +22,6 @@ public class Club {
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
-
-    @Column(nullable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "marker_id")
