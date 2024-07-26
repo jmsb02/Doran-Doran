85bda7e232a016d5c61d7b213751cfa331e00478
CHAEYEON KIM <gimchaeyeon57@gmail.com>
2024-07-26 20:40:48 +0900

    Update Service

--- src/main/java/com/dorandoran/backend/Member/Controller/MemberController.java
+++ src/main/java/com/dorandoran/backend/Member/Controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.dorandoran.backend.Member.Controller;
 
-import com.dorandoran.backend.Member.Model.MemberRepository;
 import com.dorandoran.backend.Member.Model.MemberService;
 import com.dorandoran.backend.Member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -8,27 +7,37 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
-    private final MemberRepository memberRepository;
 
     @GetMapping("/")
     public String home(Model model) {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (!email.equals("anonymousUser")) {
-            MemberDto user = (MemberDto) memberService.loadUserByUsername(email);
-            user.setPassword(null);
-            model.addAttribute("user", user);
+            MemberDto member = (MemberDto) memberService.loadUserByUsername(email);
+            member.setPassword(null);
+            model.addAttribute("member", member);
         }
-        return "index";
+        return "home";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model,
+                        @RequestParam(value="error", required = false) String error,
+                        @RequestParam(value = "exception", required = false) String exception) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        SecurityContextHolder.clearContext();
+        return "redirect:/login?logout";
     }
 }
