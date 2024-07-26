7927634d3667701b85410fd7f870be36621615dd
CHAEYEON KIM <gimchaeyeon57@gmail.com>
2024-07-26 12:35:03 +0900

    Update Service

--- src/main/java/com/dorandoran/backend/Member/Model/MemberRepository.java
+++ src/main/java/com/dorandoran/backend/Member/Model/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.dorandoran.backend.Member.Model;
 
+import com.dorandoran.backend.Member.dto.MemberDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    MemberDto findByEmail(String email);
 }
