0bea12e3042d1a6f3c237dce96924e698722ef78
CHAEYEON KIM <gimchaeyeon57@gmail.com>
2024-07-31 19:14:26 +0900

    Update Service

---
+++ src/main/java/com/dorandoran/backend/Member/domain/Address.java
@@ -0,0 +1,23 @@
+package com.dorandoran.backend.Member.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class Address {
+    private String zipcode;
+    private String streetAdr;
+    private String detailAdr;
+
+
+    public Address(String zipcode, String streetAdr, String detailAdr) {
+        this.zipcode = zipcode;
+        this.streetAdr = streetAdr;
+        this.detailAdr = detailAdr;
+    }
+
+
+}
