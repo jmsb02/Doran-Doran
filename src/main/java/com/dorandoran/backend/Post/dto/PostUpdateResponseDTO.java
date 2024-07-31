4538cb5f621b3b1fbe514a58f5258b8cb02adeba
jaexxin <jmsb02@naver.com>
2024-07-31 09:46:40 +0900

    7/30 Post Service, controller (페이징 처리 포함) - file 제외

---
+++ src/main/java/com/dorandoran/backend/Post/dto/PostUpdateResponseDTO.java
@@ -0,0 +1,23 @@
+package com.dorandoran.backend.Post.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class PostUpdateResponseDTO {
+    private Long post_id;
+    private String title;
+    private String content;
+    private Long user_id;
+    private LocalDateTime updateAt;
+
+    public PostUpdateResponseDTO(Long post_id, String title, String content, Long user_id, LocalDateTime updateAt) {
+        this.post_id = post_id;
+        this.title = title;
+        this.content = content;
+        this.user_id = user_id;
+        this.updateAt = updateAt;
+    }
+}
