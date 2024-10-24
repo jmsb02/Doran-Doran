51457d1cb9ea8bc43b49b8c7a2acada4813d749d
jaexxin <jmsb02@naver.com>
2024-10-24 12:09:38 +0900

    1024 Spring Security를 통한 MarkerController member 가져오기

--- src/main/java/com/dorandoran/backend/Marker/Controller/MarkerController.java
+++ src/main/java/com/dorandoran/backend/Marker/Controller/MarkerController.java
@@ -1,63 +1,58 @@
 package com.dorandoran.backend.Marker.Controller;
 
-import com.dorandoran.backend.Marker.Model.MarkerCommandService;
+import com.dorandoran.backend.Marker.Model.MarkerService;
 import com.dorandoran.backend.Marker.dto.*;
 
 import com.dorandoran.backend.Member.domain.Member;
-import com.dorandoran.backend.Member.domain.MemberRepository;
-import com.dorandoran.backend.Member.exception.MemberNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/marker")
 public class MarkerController {
 
-    private final MarkerCommandService markerCommandService;
-    private final MemberRepository memberRepository;
+    private final MarkerService markerService;
 
     //마커 작성
     @PostMapping
-    public ResponseEntity<MarkerResponseDTO> createPost(@RequestBody @Valid MarkerDTO markerDTO) {
-        Member member = memberRepository.findById(markerDTO.getMemberId()).orElseThrow(MemberNotFoundException::new);
-        Long markerId = markerCommandService.saveMarker(markerDTO, member);
-        MarkerResponseDTO response = new MarkerResponseDTO(markerId, "마커가 성공적으로 생성되었습니다.");
-        return ResponseEntity.ok(response);
+    public ResponseEntity<Long> createPost(@RequestBody @Valid MarkerDTO markerDTO, @AuthenticationPrincipal Member member) {
+        Long markerId = markerService.saveMarker(markerDTO, member);
+        return new ResponseEntity<>(markerId, HttpStatus.CREATED);
     }
 
     //마커 단일 조회
     @GetMapping("/{marker_id}")
-    public ResponseEntity<MarkerCheckDTO> findPostOne(@PathVariable("marker_id") Long marker_id) {
+    public ResponseEntity<MarkerFindDTO> findPostOne(@PathVariable("marker_id") Long markerId) {
         //게시물 조회
-        MarkerCheckDTO marker = markerCommandService.findMarkerOne(marker_id);
+        MarkerFindDTO marker = markerService.findMarkerOne(markerId);
 
         return ResponseEntity.ok(marker);
     }
 
     //특정 사용자 마커 조회
     @GetMapping("/user/{member_id}")
-    public ResponseEntity<List<MarkerCheckDTO>> findMarkersByUser(@PathVariable Long member_id) {
-        List<MarkerCheckDTO> markers = markerCommandService.findMarkersUsers(member_id);
+    public ResponseEntity<List<MarkerFindDTO>> findMarkersByUser(@PathVariable("member_id") Long memberId) {
+        List<MarkerFindDTO> markers = markerService.findMarkersUsers(memberId);
         return ResponseEntity.ok(markers);
     }
 
     //전체 마커 조회
     @GetMapping("/allMarkers")
     public ResponseEntity<List<MarkerFindDTO>> findAllMarkers() {
-        List<MarkerFindDTO> markers = markerCommandService.findAllMarkers();
+        List<MarkerFindDTO> markers = markerService.findAllMarkers();
         return ResponseEntity.ok(markers);
     }
 
-
+    //마커 삭제
     @DeleteMapping("/{marker_id}")
-    public ResponseEntity<DeleteMarkerResponseDTO> deleteMarker(@PathVariable Long marker_id) {
-        markerCommandService.deleteMarker(marker_id);
-        DeleteMarkerResponseDTO response = new DeleteMarkerResponseDTO("마커가 삭제되었습니다.", true);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<Void> deleteMarker(@PathVariable("marker_id") Long markerId) {
+        markerService.deleteMarker(markerId);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/dorandoran/backend/Member/domain/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.dorandoran.backend.Member.domain;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public CustomUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new SimpleUserDetails(member); // CustomUserDetails는 UserDetails를 구현한 클래스
+    }
+}
