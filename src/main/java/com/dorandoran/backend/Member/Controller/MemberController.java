e083158f48ece4fa55e2241b6c1936ee81de8e60
jaexxin <jmsb02@naver.com>
2024-10-31 23:41:29 +0900

    Spring Security를 이용한 사용자 인증 및 조회 memberController + memberService, SecurityConfig 등 수정

--- src/main/java/com/dorandoran/backend/Member/Controller/MemberController.java
+++ src/main/java/com/dorandoran/backend/Member/Controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.dorandoran.backend.Member.Controller;
 
-import com.dorandoran.backend.Member.domain.Member;
 import com.dorandoran.backend.Member.domain.MemberRepository;
 import com.dorandoran.backend.Member.domain.MemberService;
 import com.dorandoran.backend.Member.domain.SimpleUserDetails;
@@ -13,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/dorandoran/backend/Member/dto/req/SignUpDTO.java
+++ src/main/java/com/dorandoran/backend/Member/dto/req/SignUpDTO.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
